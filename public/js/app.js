dd7c306567f43a16b50f4de895e9f69b2a17b729
mmmfang <mae.fang@gmail.com>
2015-11-19 13:00:02 -0500

    api is working yay and displaying info on index page

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ app.controller('WeatherController', ['$http', function($http) {
     $http.get('https://api.worldweatheronline.com/free/v2/marine.ashx?key=45eb0c61fa43cc9be6c37ff2a3d30&format=json&q=40.64,-73.76')
     	.success(function(data) {
       		controller.data = data; //returns whole shebang
-      		controller.singular= data.data; //returns one object of 3 arrays- nearest area, request and weather 
+      		controller.dataNest= data.data; //returns one object of 3 arrays- nearest area, request and weather 
       		controller.weatherObject = data.data.weather; //returns singular weather with all properties inside
       		console.log(controller.weatherObject);
-
+      		controller.oneForecast = data.data.weather[0];
+      		console.log(controller.oneForecast);
     	}).error (function() {
     		console.log("API not gotten");
     	})
