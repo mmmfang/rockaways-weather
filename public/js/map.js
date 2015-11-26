0ccaa559cef5eea8ed7a0048bcdf06c0b860a0e1
mmmfang <mae.fang@gmail.com>
2015-11-26 17:09:59 -0500

    added a google map page using google maps api

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,18 @@
-//map options
-var options = {
-  	center: {
-    	lat: 40.64,
-   		lng: -73.76
- 	},
-	  zoom: 10
-},
-element = document.getElementbyId('map-canvas'),
-map = new google.maps.Map(element,options);
+// //map options
+// var options = {
+//   	center: {
+//     	lat: 40.64,
+//    		lng: -73.76
+//  	},
+// 	  zoom: 10
+// },
+// element = document.getElementbyId('map-canvas'),
+// map = new google.maps.Map(element,options);
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
