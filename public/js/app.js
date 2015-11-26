ed9ec7a72169efee1da6c06387c8cedad2c956dd
mmmfang <mae.fang@gmail.com>
2015-11-26 15:30:26 -0500

    took out ngAnimate since it was messing with the weather cards

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,6 @@ app.controller('WeatherController', ['$http', function($http) {
       		controller.data = data; //returns whole shebang
       		controller.dataNest= data.data; //returns one object of 3 arrays- nearest area, request and weather 
       		controller.weatherObject = data.data.weather; //returns singular weather with all properties inside
-      		console.log(controller.weatherObject);
       		controller.oneForecast = data.data.weather[0];
       		console.log(controller.oneForecast);
     	}).error (function() {
