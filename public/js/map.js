db9eeb3806564e02a361153896018f1397f0a0da
mmmfang <mae.fang@gmail.com>
2015-12-21 00:42:51 -0500

    added marker to map

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,21 @@
 
-var map;
+//var map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: {lat: 40.64, lng: -73.76},
-    zoom: 8
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 4,
+    center: {lat: -25.363882, lng: 131.044922 }
+  });
+
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
   });
 }
 
+function placeMarkerAndPanTo(latLng, map) {
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+  map.panTo(latLng);
+}
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ server.use(morgan('dev'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(bodyParser.text()); 
+server.use(bodyParser.json({type: 'application/vnd.api+json'}));
 server.use(methodOverride('_method'));
 
 
